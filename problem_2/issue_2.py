bc3771983010fd0c06306eb1c2e880246050752a
ErickCodeman93 <erickalvavazquez@gmail.com>
2021-08-26 01:22:17 -0500

    progress: issue 2 finish

--- problem_2/issue_2.py
+++ problem_2/issue_2.py
@@ -1,4 +1,34 @@
+import os
 
+def init_ ():
+
+	if os.path.exists("demogamefile.txt"):
+		# Read file 
+		f = open( "demogamefile.txt", "r" )
+
+		# markers 
+		markers = []
+
+		#Set markers
+		for x in f:
+			markers.append( x.split() )
+
+		# Close file
+		f.close()
+
+		# number of rounds
+		round = int( markers[0] [0] )
+
+		markers.pop(0)
+		# number of markers
+		markers_numbers = int( len( markers ) )
+
+		found_winner( round, markers_numbers, markers )
+		
+	else:
+		create_file( "result_error.txt", "Se necesita el archivo demogamefile.txt para iniciar el programa :<" )
+
+#Found Winner
 def found_winner( round, markers_numbers, markers ):
 
 	if round == markers_numbers:
@@ -10,50 +40,38 @@ def found_winner( round, markers_numbers, markers ):
 		for x in mylist:
 
 			if int( x[0] ) > int( x[1] ):
-				print( 'El primer jugador es lider' )
-				print( x[0] , x[1] )
-				print( 'La ventaja es de ', ( int( x[0] ) - int( x[1] ) ) )
 				firts_list.append( ( int( x[0] ) - int( x[1] ) ) )
 
 			if int( x[0] ) < int( x[1] ) :
-				print( 'El segundo jugador es lider' )
-				print( x[0] , x[1] )
-				print( 'La ventaja es de ', ( int( x[1] ) - int( x[0] ) ) )
 				two_list.append( ( int( x[1] ) - int( x[0] ) ) )
 
 		# Alghorim sorted
 		firts_list.sort()
 		firts_list.reverse()
-		print( firts_list ) 
+		
 		two_list.sort()
 		two_list.reverse()
-		print( two_list )
+		
 
 		if firts_list[0] > two_list[0]:
 			print('El primer jugador Gana por la ventaja', firts_list[0] )
+			create_file( "result.txt", "%s %s"%( str( 1 ), str( firts_list[0] ) ) )
 
 		if two_list[0] > firts_list[0]:
 			print('El segundo jugador Gana por la ventaja', two_list[0] )
+			create_file( "result.txt", "%s %s"%( str( 2 ), str( two_list[0] ) ) )
 			
 	else:
 		print('No coinciden el numero de rondas con los marcadores en el archivo')
 
+def create_file( name_file, message ):
+	if os.path.exists(name_file):
+		os.remove(name_file)
+	
+	c = open(name_file, "x")
+	c.write(message)
+	c.close()
 
-# Read file 
-f = open( "demogamefile.txt", "r" )
+#Main function
+init_()
 
-# markers 
-markers = []
-
-#Set markers
-for x in f:
-	markers.append( x.split() )
-
-# number of rounds
-round = int( markers[0] [0] )
-
-markers.pop(0)
-# number of markers
-markers_numbers = int( len( markers ) )
-
-found_winner( round, markers_numbers, markers )
